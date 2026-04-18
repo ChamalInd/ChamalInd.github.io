3f5219a921b18ddab7ab3b145b476b3b5d2d1f17
Chamal Induwara <88148370+ChamalInd@users.noreply.github.com>
2026-04-18 18:24:28 +0530

    Removed coonect stylings

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Change website theme 
 function changeTheme() {
     const icon = document.getElementById('icon');
     const theme = document.getElementById('theme-link');
@@ -12,3 +13,28 @@ function changeTheme() {
         theme.setAttribute('href', 'light.css');
     }
 }
+
+// Text animation in terminal
+const text = `const Person {
+    "name": "Chamal Induwara",
+    "age": 21,
+    "role": "CS Undergrad",
+    "focus": ["Software", "Code"]
+}`;
+let i = 0;
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function typeCode() {
+    while (true) { 
+        for (let i = 0; i < text.length; i++) {
+            document.querySelector('.text-appear').innerHTML += text.charAt(i);
+            await sleep(40); 
+        }
+
+        await sleep(2000);
+
+        document.querySelector('.text-appear').innerHTML = '';
+    }
+}
+
+typeCode()
